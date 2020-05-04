8d6fd540b3978f870e4cff082172db453f10a3d4
yejunhui <1589082159@qq.com>
2020-05-04 17:03:17 +0800

    1

--- xlsx.py
+++ xlsx.py
@@ -25,7 +25,7 @@ class ExcelPlan(object):
         '''获得系统时间'''
         self.time = time.ctime()
         '''定义计划头'''
-        self.title = ['层级','ERP编码','名称','模号','BOM用量','单位','初期','包装样式','包装数量','模穴类型','模穴数量','']
+        self.title = ['层级','ERP编码','名称','模号','BOM用量','单位','初期','包装样式','包装数量','模穴类型','模穴数量','备注']
         '''定义项目'''
         self.plan = ['需求','计划生产','实际生产','扫描入库','不良','结存']
         '''定义列标，从第9列开始'''
@@ -156,7 +156,7 @@ class ExcelPlan(object):
                     '''写公式'''
                     i = 0
                     for i in range(32) :
-                        sheet1.write_formula(row,col+i,'sumifs(%s:%s,A:A,\"总成\",B:B,%s)*INDIRECT(ADDRESS(%s,%s))'%(self.str[i+4],self.str[i+4],fn,row,5))
+                        sheet1.write_formula(row,col+i,'sumifs(%s:%s,A:A,\"总成\",B:B,%s)*INDIRECT(ADDRESS(%s,%s))'%(self.str[i+4],self.str[i+4],fn,row+1,5))
                         #sheet1.write_formula(row+1,col+i,'vlookup(%s,Summary!B:AZ,column(%s),0)'%(name,self.str[i]+'1'))
                     row += 1
                     col = 0
@@ -187,9 +187,9 @@ class ExcelPlan(object):
                     col += 1
                     i = 0
                     for i in range(0,32):
-                        sheet2.write_formula(row,col,'sumifs(生产物料推移表!%s:%s,生产物料推移表!L:L,\"计划生产\",B:B,%s)'%(self.str[i+4],self.str[i+4],li[1]))
+                        sheet2.write_formula(row,col,'sumifs(生产物料推移表!%s:%s,生产物料推移表!L:L,\"计划生产\",生产物料推移表!B:B,\"%s\")'%(self.str[i+4],self.str[i+4],li[1]))
                         col += 1
-                    sheet2.write_formula(row,col,'sum(j%s:an%s)'%(row+1,row+1))
+                    sheet2.write_formula(row,col+32,'sum(j%s:an%s)'%(row+1,row+1))
                     col = 0
                     row += 1
 
@@ -212,6 +212,7 @@ class ExcelPlan(object):
             name = '\"'+d[1]+'\"'
             for l in d :
                  sheet3.write(row,col,l)
+                 sheet3.write(row+1,col,l)
                  col += 1
             sheet3.write(row,col,self.plan[0])
             sheet3.write(row+1,col,self.plan[1])
@@ -220,16 +221,26 @@ class ExcelPlan(object):
             sheet3.write(row+4, col, self.plan[4])
             sheet3.write(row+5,col,self.plan[5])
             col += 1
-            """i = 0
+            i = 0
             for i in range(32):
                 if d[0] != '总成':
                     sheet3.write_formula(row,col,'sumifs(BOM!%s:%s,BOM!A:A,".1",BOM!B:B,\"%s\")'%(self.str[i+4],self.str[i+4],d[1]))
-                sheet3.write_formula(row,col,'=IF(NOW()<%s$3,INDIRECT(ADDRESS(%s,%s-1))-INDIRECT(ADDRESS(%s-5,%s))+INDIRECT(ADDRESS(%s-4,%s))-INDIRECT(ADDRESS(%s-1,%s)),INDIRECT(ADDRESS(%s,%s-1))-INDIRECT(ADDRESS(%s-5,%s))+INDIRECT(ADDRESS(%s-2,%s))-INDIRECT(ADDRESS(%s-1,%s))'%(self.str[i+4],row+6,col,row+6,col,row+6,col,row+6,col,row+6,col,row+6,col,row+6,col,row+6,col))
+                    if i == 0:
+                        sheet3.write_formula(row+5,col,'IF(DAY(NOW())<M3,INDIRECT(ADDRESS(%s,7))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)),INDIRECT(ADDRESS(%s,7))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)))'%(row+6,row+1,col+1,row+2,col+1,row+5,col+1,row+6,row+1,col+1,row+4,col+1,row+5,col+1))
+                    else:
+                        sheet3.write_formula(row+5,col,'IF(DAY(NOW())<M3,INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)),INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)))'%(row+6,col,row+1,col+1,row+2,col+1,row+5,col+1,row+6,col,row+1,col+1,row+4,col+1,row+5,col+1))
+                    col += 1
+                else:
+                    if i == 0:
+                        sheet3.write_formula(row+5,col,'IF(DAY(NOW())<M3,INDIRECT(ADDRESS(%s,7))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)),INDIRECT(ADDRESS(%s,7))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)))'%(row+6,row+1,col+1,row+2,col+1,row+5,col+1,row+6,row+1,col+1,row+4,col+1,row+5,col+1))
+                    else:
+                        sheet3.write_formula(row+5,col,'IF(DAY(NOW())<M3,INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)),INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s))+INDIRECT(ADDRESS(%s,%s))-INDIRECT(ADDRESS(%s,%s)))'%(row+6,col,row+1,col+1,row+2,col+1,row+5,col+1,row+6,col,row+1,col+1,row+4,col+1,row+5,col+1))
+                    col += 1
                 #sheet3.write_formula(row+1, col + i,'sumif(Plan!B:B,%s,Plan!%s:%s)'%(name,self.str[i+1],self.str[i+1]))
                 #sheet3.write_formula(row+2, col + i,'indirect(address(%s,%s))+indirect(address(%s,%s))-indirect(address(%s,%s))' % ( row + 3, col+ i, row+1, col + i+1, row + 2, col + i+1))
             sheet3.write_formula(row,col+31,'sum(j%s:an%s)'%(row+1,row+1))
             row += 6
-            col = 0"""
+            col = 0
 
         f.close()
         print('写入成功，文件保存在：%s，名为%s的Excel(.xlsx)y文件'%(self.arg,'生产推移表'))
